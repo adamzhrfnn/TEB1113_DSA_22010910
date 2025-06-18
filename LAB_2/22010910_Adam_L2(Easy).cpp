bf791a4983fe3c6f39ecf7628dfa63c860859afe
adamzhrfnn <adamasri72@gmail.com>
2025-06-18 11:11:47 +0800

    Add files via upload

---
+++ LAB_2/22010910_Adam_L2(Easy).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Matrix{
+    public:
+    void multiply(int m1[2][2], int m2[2][2]){
+        int r1 = 2, c1 = 2, r2 = 2, c2 = 2;
+        int sum[r1][c2];
+
+        if (c1 != r2){
+            cout << "Invalid Multiplication";
+            return;
+        }
+
+        for (int i = 0; i < 2; i++){
+            for (int j = 0; j < 2; j++){
+                for (int k = 0; k < 2; k++){
+                    sum[i][j] += m1[i][k] * m2[k][j];
+                }
+            }
+        }
+
+        cout << "Result of Multiplying Matrices: " << endl;
+        for (int i = 0; i < 2; i++){
+            for (int j = 0; j < 2; j++){
+                cout << sum[i][j] << "  ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main(){
+    int m1[2][2] = {{1,1},{2,2}};
+    int m2[2][2] = {{1,1},{2,2}};
+
+    Matrix matrix1;
+    matrix1.multiply(m1,m2);
+}
