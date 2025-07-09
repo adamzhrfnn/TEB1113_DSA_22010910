38900f94cc89172ff7fbfafd463ea4e8ceb829d4
adamzhrfnn <adamasri72@gmail.com>
2025-07-09 18:08:17 +0800

    Add files via upload

---
+++ LAB_6/22010910_Adam_L6.1.cpp
@@ -0,0 +1,90 @@
+/*
+*****************************
+id: 22010910
+name: Adam Zharfan
+contact: 0129520190
+email: muhammad_22010910@utp.edu.my
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Queue {
+private:
+    static const int SIZE = 100; 
+    string arr[SIZE];
+    int front;
+    int rear;
+
+public:
+    Queue() {
+        front = -1;
+        rear = -1;
+    }
+
+    void enqueue(string name) {
+        if (rear == SIZE - 1) {
+            cout << "Queue is full. Cannot enqueue." << endl;
+            return;
+        }
+
+        if (front == -1) {
+            front = 0;
+        }
+
+        rear++;
+        arr[rear] = name;
+    }
+
+    void dequeue() {
+        if (is_empty()) {
+            cout << "Queue is empty. Cannot dequeue." << endl;
+            return;
+        }
+
+        cout << "Dequeued: " << arr[front] << endl;
+
+        if (front == rear) {
+            front = -1;
+            rear = -1;
+        } else {
+            front++;
+        }
+    }
+
+    void display_queue() {
+        if (is_empty()) {
+            cout << "Queue is empty." << endl;
+            return;
+        }
+
+        for (int i = front; i <= rear; i++) {
+            cout << arr[i] << " -> ";
+        }
+        cout << "NULL" << endl;
+    }
+
+    bool is_empty() {
+        return front == -1 || front > rear;
+    }
+};
+
+int main() {
+    cout << "Queue" << endl;
+    Queue q;
+
+    q.enqueue("Ali");
+    q.enqueue("Ahmed");
+    q.enqueue("Alee");
+
+    cout << "Queue: ";
+    q.display_queue();
+
+    q.dequeue();
+
+    cout << "After dequeueing: " << endl;
+    q.display_queue();
+
+    return 0;
+}
